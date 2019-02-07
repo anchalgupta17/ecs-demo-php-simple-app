9be0b5eba4662c45dc506b0d21c692bce49376b4
Anchal Gupta <AnchalG@hcl.com>
2019-02-07 11:33:41 +0530

    anchala

--- src/index.php
+++ src/index.php
@@ -17,7 +17,7 @@
             <div class="hero-unit">
                 <h1>Simple PHP App</h1>
                 <h2>Congratulations</h2>
-                <p>Your PHP application is now running on a container in Amazon ECS.This was made by Anchal Gupta</p>
+                <p>Your PHP application is now running on a container in Amazon ECS.Gully Boy</p>
                 <p>The container is running PHP version <?php echo phpversion(); ?>.</p>
                 <?php
                         $myfile = fopen("/var/www/my-vol/date", "r") or die("");
